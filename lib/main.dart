bf025cadf4bb72273b1cd6404d2d182b8cd8eb61
jyo <bharathiinstitute1@gmail.com>
2025-06-30 14:15:37 +0530

    jyoo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+// container 
 void main() {
   runApp(const MyApp());
 }
@@ -15,6 +15,16 @@ class MyApp extends StatelessWidget {
           width: double.infinity,
           height: double.infinity,
           color: Colors.blue, // Change color as needed
+          child: const Center(
+            child: Text(
+              'Hello, Flutter!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
     );
